c412442d9ac49c7cb3165ae47d7df8cd7a17a4cf
fulsep <57238803+fulsep@users.noreply.github.com>
2025-10-08 16:15:46 +0700

    fix: modal animation and y-offset based on screen height divided by 2

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -8,12 +8,14 @@ const Modal = (props)=>{
   }
   const animatedDiv = React.createRef()
   React.useEffect(()=>{
-    animatedDiv.current.classList.remove("-translate-y-50")
-    animatedDiv.current.classList.add("translate-y-0")
+    setTimeout(()=>{
+      animatedDiv.current.classList.remove("-translate-y-[calc(theme(height.screen)/2)]")
+      animatedDiv.current.classList.add("translate-y-0")
+    })
   },[animatedDiv])
   return (
     <div className='absolute z-[9999] top-0 left-0 max-w-md w-full h-screen bg-black/50 flex justify-center items-center'>
-      <div ref={animatedDiv} className='w-full mx-10 transition-all duration-500 -translate-y-50 bg-white p-5 py-5 rounded flex flex-col gap-3'>
+      <div ref={animatedDiv} className='w-full mx-10 transition-all duration-500 -translate-y-[calc(theme(height.screen)/2)] bg-white p-5 py-5 rounded flex flex-col gap-3'>
         <div className='flex justify-between'>
           <h1 className='font-bold text-lg'>{props.title}</h1>
           <div>
