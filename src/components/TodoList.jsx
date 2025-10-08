6da1f9c98b788bc0e93955db2fc00dfee67bcd41
fulsep <57238803+fulsep@users.noreply.github.com>
2025-10-08 16:40:45 +0700

    feat: add dbl-click handle to show options (to support mobile accessibility)

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -16,24 +16,35 @@ function TodoList() {
     <DeleteModalContext.Provider value={{showModal, setShowModal}}>
       <div className='flex flex-col gap-5 mb-24'>
         {todoCtx.data.map((todo,idx) => {
-          return (
-            <div className='bg-blue-500 text-white p-5 rounded relative group'>
-              <div className='flex justify-between'>
-                <div className='font-bold'>{todo.title}</div>
-                <div>{todo.time}</div>
-              </div>
-              <div className='whitespace-pre'>{todo.body}</div>
-              <div className='group-hover:flex absolute top-0 left-0 hidden flex-col px-5 justify-center items-end w-full h-full bg-linear-to-r from-transparent to-blue-500'>
-                <button onClick={()=>toggleModal(idx)} className='text-red-300'>
-                  <FaTrash size={28} />
-                </button>
-              </div>
-            </div>
-          )
+          return (<TodoItem todo={todo} index={idx} onAction={()=>toggleModal(idx)} />)
         })}
         <DeleteModal item={selectedItem} />
       </div>
     </DeleteModalContext.Provider>
+  )
+}
+
+const TodoItem = (props)=>{
+  const [showOptions, setShowOptions] = React.useState(false)
+  return(
+    <div onDoubleClick={()=>setShowOptions(!showOptions)} className='bg-blue-500 select-none text-white p-5 rounded relative group'>
+      <div className='flex justify-between'>
+        <div className='font-bold'>{props.todo.title}</div>
+        <div>{props.todo.time}</div>
+      </div>
+      <div className='whitespace-pre'>{props.todo.body}</div>
+      {showOptions ? <div onDoubleClick={()=>setShowOptions(!showOptions)} className='flex absolute top-0 left-0 flex-col px-5 justify-center items-end w-full h-full bg-linear-to-r from-transparent to-blue-500'>
+        <button onClick={()=>props.onAction(props.index)} className='text-red-300'>
+          <FaTrash size={28} />
+        </button>
+      </div> : <>
+        <div className='group-hover:flex absolute top-0 left-0 hidden flex-col px-5 justify-center items-end w-full h-full bg-linear-to-r from-transparent to-blue-500'>
+          <button onClick={()=>props.onAction(props.index)} className='text-red-300'>
+            <FaTrash size={28} />
+          </button>
+        </div>
+      </>}
+    </div>
   )
 }
 
