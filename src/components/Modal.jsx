52d0cfa33bb668c6c1fbc897b89a76b27019e217
fulsep <57238803+fulsep@users.noreply.github.com>
2025-10-08 16:07:48 +0700

    fix: navbar not covered with modal backdrop

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -12,7 +12,7 @@ const Modal = (props)=>{
     animatedDiv.current.classList.add("translate-y-0")
   },[animatedDiv])
   return (
-    <div className='absolute top-0 left-0 max-w-md w-full h-screen bg-black/50 flex justify-center items-center'>
+    <div className='absolute z-[9999] top-0 left-0 max-w-md w-full h-screen bg-black/50 flex justify-center items-center'>
       <div ref={animatedDiv} className='w-full mx-10 transition-all duration-500 -translate-y-50 bg-white p-5 py-5 rounded flex flex-col gap-3'>
         <div className='flex justify-between'>
           <h1 className='font-bold text-lg'>{props.title}</h1>
