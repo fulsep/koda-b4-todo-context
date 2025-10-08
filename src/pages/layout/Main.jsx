b66ee83e1686acf726bf17bd54cb1663ee76a0d0
fulsep <57238803+fulsep@users.noreply.github.com>
2025-10-08 14:54:19 +0700

    feat: reusable modal creation, splitting todo form into separated file

--- src/pages/layout/Main.jsx
+++ src/pages/layout/Main.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 import { Outlet } from 'react-router-dom'
 import Navbar from '../../components/Navbar'
 import ModalContext from '../../components/ModalContext'
+import Modal from '../../components/Modal'
 import TodoContext from '../../components/TodoContext'
+import AddTodoForm from '../../components/AddTodoForm'
 
 function Main() {
   const [showModal, setShowModal] = React.useState(false)
@@ -16,7 +18,7 @@ function Main() {
               <Outlet />
             </div>
             <Navbar />
-            {showModal && <Modal />}
+            {showModal && <AddTodoModal />}
           </div>
         </div>
       </ModalContext.Provider>
@@ -24,63 +26,13 @@ function Main() {
   )
 }
 
-const Modal = ()=>{
+const AddTodoModal = ()=>{
   const modalCtx = React.useContext(ModalContext)
   const todoCtx = React.useContext(TodoContext)
-  const formRef = React.useRef()
-  const handleData = (e)=>{
-    e.preventDefault()
-    const {value: title} = e.target.title
-    const {value: body} = e.target.body
-    const {value: time} = e.target.time
-    todoCtx.setData([
-      ...todoCtx.data,
-      {title,body,time}
-    ])
-    toggleModal()
-  }
-  const toggleModal = ()=>{
-    modalCtx.setShowModal(!modalCtx.showModal)
-  }
-  const animatedDiv = React.createRef()
-  React.useEffect(()=>{
-    animatedDiv.current.classList.remove("-translate-y-50")
-    animatedDiv.current.classList.add("translate-y-0")
-  },[animatedDiv])
-  return (
-    <div className='absolute top-0 left-0 max-w-md w-full h-screen bg-black/50 flex justify-center items-center'>
-      <div ref={animatedDiv} className='w-full mx-10 transition-all duration-500 -translate-y-50 bg-white p-5 py-5 rounded flex flex-col gap-3'>
-        <h1 className='font-bold text-lg'>Add New Todo</h1>
-        <form ref={formRef} onSubmit={handleData} className='flex flex-col gap-2'>
-          <div className='flex flex-col gap-1'>
-            <label htmlFor="title">Title</label>
-            <input autoFocus className='h-12 border rounded px-3' type="text" name='title' id='title' />
-          </div>
-          <div className='flex flex-col gap-1'>
-            <label htmlFor="body">Body</label>
-            <textarea className='border rounded p-3 h-24' type="text" name='body' id='body' />
-          </div>
-          <div className='flex flex-col gap-1'>
-            <label htmlFor="time">Time</label>
-            <select className='border h-12 rounded px-3' name="time" id="time">
-              {[...Array(24)].map((_,hour) => {
-                const strHour = hour.toString()
-                const len = strHour.length
-                return(<option>{len>1?strHour:'0'+strHour}:00</option>)
-              })}
-            </select>
-          </div>
-          <div className='flex justify-end gap-5'>
-            <button className='text-black  px-5 py-2 rounded' type="button" onClick={toggleModal}>
-              <span>Cancel</span>
-            </button>
-            <button className='text-white bg-blue-500 px-5 py-2 rounded' type="submit">
-              <span>Save</span>
-            </button>
-          </div>
-        </form>
-      </div>
-    </div>
+  return(
+    <Modal title="Add New Todo">
+      <AddTodoForm todoCtx={todoCtx} onClose={()=>modalCtx.setShowModal(false)} />
+    </Modal>
   )
 }
 
