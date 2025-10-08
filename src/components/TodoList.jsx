728e9dc62e6a9b0349de632bbb201ed080d4bb78
Anggiprayoga28 <anggiprayoga930@gmail.com>
2025-10-08 19:29:29 +0700

    Feat: Integrate EditModal for editing todos and enhance TodoItem actions

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,25 +1,37 @@
 import React, { useContext } from 'react'
 import TodoContext from './TodoContext'
-import { FaTrash } from 'react-icons/fa6'
-import {DeleteModalContext} from './ModalContext'
+import { FaTrash, FaPen } from 'react-icons/fa6'
+import {DeleteModalContext, EditModalContext} from './ModalContext'
 import Modal from './Modal'
+import AddTodoForm from './AddTodoForm'
 
 function TodoList() {
   const [showModal, setShowModal] = React.useState(false)
+  const [showEditModal, setShowEditModal] = React.useState(false)
   const todoCtx = React.useContext(TodoContext)
   const [selectedItem, setSelectedItem] = React.useState(null)
+
   const toggleModal = (idx)=>{
     setShowModal(!showModal)
     setSelectedItem(idx)
   }
+
+  const toggleEditModal = (idx)=>{
+    setShowEditModal(!showEditModal)
+    setSelectedItem(idx)
+  }
+
   return (
     <DeleteModalContext.Provider value={{showModal, setShowModal}}>
-      <div className='flex flex-col gap-5 mb-24'>
-        {todoCtx.data.map((todo,idx) => {
-          return (<TodoItem todo={todo} index={idx} onAction={()=>toggleModal(idx)} />)
-        })}
-        <DeleteModal item={selectedItem} />
-      </div>
+      <EditModalContext.Provider value={{showModal: showEditModal, setShowModal: setShowEditModal}}>
+        <div className='flex flex-col gap-5 mb-24'>
+          {todoCtx.data.map((todo,idx) => {
+            return (<TodoItem key={idx} todo={todo} index={idx} onDelete={()=>toggleModal(idx)} onEdit={()=>toggleEditModal(idx)} />)
+          })}
+          <DeleteModal item={selectedItem} />
+          <EditModal item={selectedItem} />
+        </div>
+      </EditModalContext.Provider>
     </DeleteModalContext.Provider>
   )
 }
@@ -34,12 +46,18 @@ const TodoItem = (props)=>{
       </div>
       <div className='whitespace-pre'>{props.todo.body}</div>
       {showOptions ? <div onDoubleClick={()=>setShowOptions(!showOptions)} className='flex absolute top-0 left-0 flex-col px-5 justify-center items-end w-full h-full bg-linear-to-r from-transparent to-blue-500'>
-        <button onClick={()=>props.onAction(props.index)} className='text-red-300'>
+        <button onClick={()=>props.onEdit(props.index)} className='text-red-300'>
+          <FaPen size={28} />
+        </button>
+        <button onClick={()=>props.onDelete(props.index)} className='text-red-300'>
           <FaTrash size={28} />
         </button>
       </div> : <>
-        <div className='group-hover:flex absolute top-0 left-0 hidden flex-col px-5 justify-center items-end w-full h-full bg-linear-to-r from-transparent to-blue-500'>
-          <button onClick={()=>props.onAction(props.index)} className='text-red-300'>
+        <div className='group-hover:flex absolute top-0 left-0 hidden flex-col gap-3 px-5 justify-center items-end w-full h-full bg-linear-to-r from-transparent to-blue-500'>
+          <button onClick={()=>props.onEdit(props.index)} className='text-red-300'>
+            <FaPen size={28} />
+          </button>
+          <button onClick={()=>props.onDelete(props.index)} className='text-red-300'>
             <FaTrash size={28} />
           </button>
         </div>
@@ -69,6 +87,26 @@ const DeleteModal = (props)=>{
             <span>Yes</span>
           </button>
         </div>
+      </Modal>
+    )
+  }
+  return (<></>)
+}
+
+const EditModal = (props)=> {
+  const todoCtx = useContext(TodoContext)
+  const modalCtx = useContext(EditModalContext)
+  
+  if(modalCtx.showModal && props.item !== null){
+    return (
+      <Modal modalCtx={modalCtx} title="Edit Todo">
+        <AddTodoForm 
+          todoCtx={todoCtx} 
+          editMode={true}
+          editIndex={props.item}
+          editData={todoCtx.data[props.item]}
+          onClose={()=>modalCtx.setShowModal(false)} 
+        />
       </Modal>
     )
   }
