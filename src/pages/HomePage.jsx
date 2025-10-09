448706c9f8f6afc41d12e2a8c49532bcf41d0f38
VirgiliIw <imanuelambar11@gmail.com>
2025-10-09 08:17:52 +0700

    feat: make edit feature to rewrite the task

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,34 +1,22 @@
 import moment from 'moment'
 import React from 'react'
 import TodoContext from '../components/TodoContext'
+import TodoList from '../components/TodoList'
 
 function HomePage() {
   const [selectedDay, setDay] = React.useState(moment().date().toString())
   const weeks = ["Mon", "Tue", "Wed","Thu", "Fri", "Sat", "Sun"]
-  const [showMessage,setShowMessage] = React.useState("")
   const todoCtx = React.useContext(TodoContext)
-  const handleDelete = (id) => {
-    todoCtx.deleteTodo(id)
-    setShowMessage("Delete Success 😎✔️")
-    setTimeout(() => {
-      setShowMessage("")
-    }, 3000)
-  }
   return (
     <>
       <div className='sticky top-0 px-10 py-5 flex flex-col gap-5'>
         <div>{moment().format("MMM DD, YYYY")}</div>
         <div className='text-3xl font-bold'>Today</div>
-        {showMessage && (
-          <div className='absolute top-95 right-31 bg-green-500 hover:bg-red-400 text-white py-4 rounded-lg animate-bounce'>
-            {showMessage}
-          </div>
-        )}
         <div className='flex'>
           {weeks.map((day, index) => {
             const getDay = moment().day(index+1).date().toString()
             return(
-              <button onClick={()=>setDay(getDay)} className='hover:cursor-pointer flex-1 flex flex-col items-center [&>*:first-child]:text-gray-500 [&>*:last-child]:font-bold' key={index}>
+              <button onClick={()=>setDay(getDay)} className='hover:cursor-pointer flex-1 flex flex-col items-center [&>*:first-child]:text-gray-500 [&>*:last-child]:font-bold'>
                 <span className={selectedDay === getDay ? 'text-[theme(color.blue.500)!important] font-bold':''}>{day}</span>
                 <span className={selectedDay === getDay ? 'text-[theme(color.blue.500)!important] font-bold':''}>{getDay}</span>
               </button>)
@@ -38,23 +26,7 @@ function HomePage() {
       {todoCtx.data.length === 0 && <div className='flex-1 flex justify-center items-center'>
         <span>No Activity!</span>
       </div>}
-      <div className='flex flex-col gap-5 mb-24'>
-        {todoCtx.data.map(todo => {
-          return (
-            <div className='bg-blue-500 text-white p-5 rounded 
-            hover:cursor-pointer transform transition-all duration-500 hover:scale-105' key={todo.id}>
-              <div className='flex justify-between'>
-                <div className='font-bold'>{todo.title}</div>
-                <div>{todo.time}</div>
-              </div>
-              <div className='flex justify-between'>
-                <div className='whitespace-pre'>{todo.body}</div>
-                <button onClick={()=>handleDelete(todo.id)} className='hover:text-black cursor-pointer'>Delete</button>
-              </div>
-            </div>
-          )
-        })}
-      </div>
+      {todoCtx.data.length > 0 && <TodoList />}
     </>
   )
 }
